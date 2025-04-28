2b7ce4516caec333801e0c63cde6592dbb347074
Maksim <Maksim.ildeev.2007@yandex.ru>
2025-04-28 14:50:28 +0300

    fixed

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,14 @@
+from typing import List, Dict, Any
 
 
-from typing import List, Dict
 
-
-def filter_by_state(info_list: List[Dict], state: str = "EXECUTED") -> List[Dict]:
+def filter_by_state(operations: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
     '''Фильтрует список словарей по заданному значению поля "state".'''
 
-    return [item for item in info_list if item.get("state") == state]
+    return [operation for operation in operations if operation.get("state") == state]
 
 
-def sort_by_date(info_list: List[Dict]) -> List[Dict]:
+def sort_by_date(operations: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
     '''Сортирует список словарей по убыванию даты.'''
 
-    return sorted(info_list, key=lambda item: item["date"], reverse=True)
+    return sorted(operations, key=lambda x: x.get("date", ""), reverse=reverse)
