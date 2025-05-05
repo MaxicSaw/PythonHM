8e6c806bc870711eed9f1f14159a9be22cbb3560
Maksim <Maksim.ildeev.2007@yandex.ru>
2025-05-05 12:55:44 +0300

    fixed

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,20 +1,6 @@
 import pytest
 
-
-from typing import List, Dict
-
-
-def filter_by_state(info_list: List[Dict], state: str = "EXECUTED") -> List[Dict]:
-    '''Фильтрует список словарей по заданному значению поля "state".'''
-
-    return [item for item in info_list if item.get("state") == state]
-
-
-def sort_by_date(info_list: List[Dict]) -> List[Dict]:
-    '''Сортирует список словарей по убыванию даты.'''
-
-    return sorted(info_list, key=lambda item: item["date"], reverse=True)
-
+from src.processing import filter_by_state
 
 @pytest.mark.parametrize("my_list, state, expected", [
     (
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,26 +1,10 @@
 import pytest
-from src import masks
+from src.widget import mask_account_card
 
 
 @pytest.mark.parametrize('value_card, expected_card', [
-    ('7000792289606361', '7000 79** **** 6361'),
-    ('ffasfdasfasdvav', 'Номер карты введен не корректно')
+    ('Visa 7000792289606361', 'Visa 7000 79** **** 6361'),
+    ('Счет 12341234123412341234', 'Счет **1234')
 ])
 def test_mask_number_card(value_card, expected_card):
     assert mask_account_card(value_card) == expected_card
-
-
-def mask_account_card(number_card: str) -> str:
-    if not number_card or not isinstance(number_card, str):
-        raise ValueError("Ошибка ввода. Ожидается строка с номером карты или счета.")
-
-    number = "".join(filter(str.isdigit, number_card))
-
-    if len(number) == 16 or len(number) == 19:
-        masked_number = masks.get_mask_card_number(number)
-    elif len(number) >= 20:
-        masked_number = masks.get_mask_account(number)
-    else:
-        return 'Номер карты введен не корректно'
-
-    return masked_number
