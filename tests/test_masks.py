997139c1c4535e44c9b18ce136ff85c060efb9a4
Maksim <Maksim.ildeev.2007@yandex.ru>
2025-05-17 15:48:05 +0300

    fixed

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,35 +1,41 @@
 import pytest
-from src import masks
+
+from src.masks import mask_account, mask_card
 
 
-@pytest.fixture
-def get_mask_card_number(number_card: str) -> str:
-    """Функцию маскировки номера банковской карты"""
-    str_number_card = number_card
-    masked = str_number_card[:6] + "*" * 6 + str_number_card[-4:]
+class TestMasks:
+    @pytest.mark.parametrize(
+        "card_number, expected_mask",
+        [
+            ("1234567890123456", "1234 **** **** 3456"),
+            ("7675496749576475", "7675 **** **** 6475"),
+            ("1234 5678 9012 3456", "1234 **** **** 3456"),  # С пробелами
+            ("123456789012345", "1234 **** **** 2345"),  # Неполная длина
+            ("123", "123"),  # Менее 16 цифр
+            ("", ""),  # Пустая строка
+            (None, None),  # None
+        ],
+    )
+    def test_get_mask_card_number(self, card_number, expected_mask):
+        assert mask_card(card_number) == expected_mask
 
-    count = 0
-    slice_masc = ""
-    for i in masked:
-        count += 1
-        slice_masc += i
-        if count % 4 == 0:
-            slice_masc += " "
+    @pytest.mark.parametrize(
+        "account_number, expected_mask",
+        [
+            ("12345678901234567890", "****7890"),
+            ("1234567890", "****7890"),  # Меньше 20 символов
+            ("123", "123"),  # Меньше 4 цифр
+            ("", ""),  # Пустая строка
+            (None, None),  # None
+        ],
+    )
+    def test_get_mask_account(self, account_number, expected_mask):
+        assert mask_account(account_number) == expected_mask
 
-    return slice_masc[0:19]
+    def test_get_mask_card_number_type_error(self):
+        with pytest.raises(TypeError):
+            mask_card(1234567890123456)  # Передача числа вместо строки
 
-
-def mask_account_card(number_card: str) -> str:
-    if not number_card or not isinstance(number_card, str):
-        raise ValueError("Ошибка ввода. Ожидается строка с номером карты или счета.")
-
-    number = "".join(filter(str.isdigit, number_card))
-
-    if len(number) == 16 or len(number) == 19:
-        masked_number = masks.get_mask_card_number(number)
-    elif len(number) >= 20:
-        masked_number = masks.get_mask_account(number)
-    else:
-        return 'Номер карты введен не корректно'
-
-    return masked_number
+            def test_get_mask_account_type_error(self):
+                with pytest.raises(TypeError):
+                    mask_account(12345678901234567890)  # Передача числа вместо строки
